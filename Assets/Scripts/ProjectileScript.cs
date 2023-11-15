1a58ff03318aff8f8351e0527fe50fad8cad1c3d
Paul Lian <49074561+vk3002h@users.noreply.github.com>
2023-11-15 11:51:49 +0800

    Revert "More Turrets(Tag Sensitive)"
    This reverts commit 42f2f3695a4baa634559793ebc2bd7fd4dd64e6d.

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+    public int damage = 1;
+
+    private float completion;
+    private float flightTime;
+    private float elapsedTime;
+    private float distance;
+
+    public DamageType damageType;
+
+    private void Start()
+    {
+        distance = Vector3.Distance(transform.position, target.position);
+    }
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        flightTime = distance / speed;
+        float completion = elapsedTime / flightTime;
+        if (target == null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, completion);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        collision.gameObject.GetComponent<EnemyScript>().TakeDamage(damage, damageType);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TurretProto.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TurretProto : MonoBehaviour
+{
+    [SerializeField] 
+    private GameObject turretProjectile;
+    [SerializeField]
+    private GameManager gameManager;
+    [SerializeField]
+    private float projectileSpeed = 20f;
+
+    public DamageType turretDamageType;
+
+    public bool boosted = false;
+    public float turretRange = 10f;
+    public float turretReload = 4f;
+
+
+    private float timer = 0f;
+    // Start is called before the first frame update
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        UnityEditor.Handles.color = Color.red;
+        UnityEditor.Handles.DrawWireDisc(transform.position, transform.forward, turretRange);
+    }
+#endif
+    private void Start()
+    {
+        gameManager = GameManager.main;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        foreach (GameObject enemy in gameManager.enemiesList)
+        {
+            if (enemy) 
+            {
+                if (Vector3.Distance(transform.position, enemy.transform.position) <= turretRange)
+                {
+                    turretFiring(enemy);
+                }
+                else if ((Vector3.Distance(transform.position, enemy.transform.position) < Vector3.Distance(transform.position, enemy.transform.position)) 
+                    && enemy.GetComponent<EnemyScript>().hitPoints > enemy.GetComponent<EnemyScript>().hitPoints)
+                {
+                    turretFiring(enemy);
+                }
+            }
+                
+        }
+
+    }
+
+    void turretFiring(GameObject turretTarget)
+    {
+        if (turretTarget != null)
+        {
+            Vector3 direction = turretTarget.transform.position - transform.position;
+
+            float RotationZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(0,0,RotationZ);
+
+            if (timer < 0f)
+            {
+                var projectile = Instantiate(turretProjectile, transform.position, transform.rotation);
+                ProjectileScript projectileScript = projectile.GetComponent<ProjectileScript>();
+
+                projectileScript.target = turretTarget.transform;
+
+                projectileScript.speed = projectileSpeed;
+                projectileScript.damageType = turretDamageType;
+
+                timer = turretReload;
+            }
+        }
+    }
+}
